400fba6cf88d4985ddfcbed69353bec6d395951f
gabrieldiaslm <xgabrieldias777@gmail.com>
2023-09-05 17:55:31 -0300

    final do dia 05/09/2023

--- beecrowd/bin/1168/main.dart
+++ beecrowd/bin/1168/main.dart
@@ -1,37 +1,42 @@
 import "dart:io";
 
-/*
-var digits = {
-  0:6,
-  1:2,
-  2:5,
-  3:5,
-  4:4,
-  5:5,
-  6:6,
-  7:3,
-  8:7,
-  9:6,
-}
- */
 void main() {
   int cases = int.parse(stdin.readLineSync() ?? '0');
 
   while (cases-- > 0) {
-    int number = int.parse(stdin.readLineSync() ?? '0');
+    var number = stdin.readLineSync() ?? '0';
 
     print(ledsCounter(number));
   }
 }
 
-int ledsCounter(int number) {
-  const digits = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6];
+int ledsCounter(String number) {
+  const digits = {
+    '0': 6,
+    '1': 2,
+    '2': 5,
+    '3': 5,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 3,
+    '8': 7,
+    '9': 6,
+  };
   int leds = 0;
 
+  for (int i = 0; i < number.length; i++) {
+    leds += digits[number[i]] ?? 0;
+    // leds += digits[number[i]]!;
+  }
+  return leds;
+}
+  /*
   do {
     leds += digits[number % 10];
     number = number ~/ 10;
   } while (number > 0);
 
   return leds;
-}
+  }
+*/
