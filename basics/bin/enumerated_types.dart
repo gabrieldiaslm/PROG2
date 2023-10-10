f91f9ea6549f51610f143b1d8a3b44ee33caf8d8
gabrieldiaslm <xgabrieldias777@gmail.com>
2023-10-10 17:31:38 -0300

    10/10/2023

---
+++ basics/bin/enumerated_types.dart
@@ -0,0 +1,9 @@
+enum Color { red, green, blue }
+
+void main() {
+  final favoriteColor = Color.blue;
+  if (favoriteColor == Color.blue) {
+    print('Your favorite color is blue!');
+    print(Color.green.name);
+  }
+}
